26c5b7f5a90ec92f34e07aa3fc53bbbf180c7b88
Amber Yust <amber.yust@gmail.com>
2014-01-29 22:55:41 -0800

    Add MutationObservers.
    By using the MutationObserver API, TrueRL is able to monitor in
    real-time for updates to the DOM, fixing links as they're added.

--- truerl.js
+++ truerl.js
@@ -4,27 +4,69 @@
   var urlPattern = /^(?:https?|ftp|file):\/\/\S+$|^[a-zA-Z0-9.-]+\.[a-z]{2,4}(?:\/\S*)?$/;
   var schemePattern = /^[a-zA-Z0-9+.-]+(?=:\/\/)/;
 
-  var makeUrl = function(text) {
-    if(schemePattern.test(text)) {
-      return text;
+  var enforceUrl = function(element, url) {
+    if(!schemePattern.test(url)) {
+      url = "http://" + url;
     }
-    return "http://" + text;
+	var oldUrl = element.getAttribute("href");
+	if(url != oldUrl) {
+	  element.setAttribute("href", url);
+	  if(urlPattern.test(element.getAttribute("title"))) {
+		element.setAttribute("title", url);
+	  }
+	}
+  }
+  
+  var trueUrl = function(element, oldUrl) {
+	var title = element.getAttribute("title");
+	var innerText = element.innerText;
+	
+	if(urlPattern.test(innerText)) {
+	  return enforceUrl(element, innerText);
+	}
+	
+	if(urlPattern.test(title)) {
+	  return enforceUrl(element, title);
+	}
+	
+	if(urlPattern.test(oldUrl)) {
+	  var newUrl = element.getAttribute("href");
+	  if(newUrl.indexOf(oldUrl) >= 0 ||
+	     newUrl.indexOf(encodeURIComponent(oldUrl)) >= 0) {
+		return enforceUrl(element, oldUrl);
+	  }
+	}
   }
 
   // Replace the hrefs for any link that has a url-like content
   var allLinks = document.getElementsByTagName("a");
   for(var i = 0; i < allLinks.length; ++i) {
-    var elem = allLinks[i];
-    var title = elem.getAttribute("title");
-    var innerText = elem.innerText;
-    if(urlPattern.test(innerText)) {
-      elem.setAttribute("href", makeUrl(innerText));
-      // Also update the title to match, if it was a url
-      if(urlPattern.test(title)) {
-        elem.setAttribute("title", innerText);
-      }
-    } else if(urlPattern.test(title)) {
-      elem.setAttribute("href", makeUrl(title));
-    }
+    trueUrl(allLinks[i]);
   }
+  
+  var attributeObserver = new MutationObserver(function(mutations) {
+	for(var i = 0; i < mutations.length; ++i) {
+	  trueUrl(mutations[i].target, mutations[i].oldValue);
+	}
+  });
+  attributeObserver.observe(document.getElementsByTagName("html")[0], {
+	subtree: true,
+	attributes: true,
+	attributeOldValue: true,
+	attributeFilter: ["href"]
+  });
+  
+  var nodeObserver = new MutationObserver(function(mutations) {
+	for(var i = 0; i < mutations.length; ++i) {
+	  var childLinks = mutations[i].target.getElementsByTagName("a");
+	  for(var j = 0; j < childLinks.length; ++j) {
+	    trueUrl(childLinks[j]);
+	  }
+	}
+  });
+  nodeObserver.observe(document.getElementsByTagName("html")[0], {
+	subtree: true,
+	childList: true,
+  });
+  
 })();
